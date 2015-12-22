a9c408cb5ee3e16b421fce26846a12545ff673f3
Matthias Stevens <matthias.stevens@gmail.com>
2015-12-22 17:11:44 +0100

    Fix for Sapelli projects uploaded prior to geokey_sapelli v0.6.1 Thes have an invalid contribution permission setting.
    Signed-off-by: Matthias Stevens <matthias.stevens@gmail.com>

---
+++ geokey_sapelli/migrations/0014_fix_everyone_contributes.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+from geokey.projects.base import EVERYONE_CONTRIB
+
+
+def fix_everyone_contributes(apps, schema_editor):
+    SapelliProject = apps.get_model('geokey_sapelli', 'SapelliProject')
+    for sapelli_project in SapelliProject.objects.all():
+        if sapelli_project.geokey_project.everyone_contributes == u'False':
+            sapelli_project.geokey_project.everyone_contributes = EVERYONE_CONTRIB.false
+            sapelli_project.geokey_project.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('geokey_sapelli', '0013_sapdownloadqrlink'),
+    ]
+    
+    operations = [
+        migrations.RunPython(fix_everyone_contributes),
+    ]
