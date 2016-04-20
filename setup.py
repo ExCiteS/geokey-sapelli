eaa4246b327bceb0741395cd9a587512b783ec06
Julius Osokinas <j.osokinas@mappingforchange.org.uk>
2016-04-20 13:34:43 +0100

    Moved requirements from separate file to main setup

--- setup.py
+++ setup.py
@@ -8,21 +8,6 @@ name = 'geokey-sapelli'
 version = __import__(name.replace('-', '_')).__version__
 repository = join('https://github.com/ExCiteS', name)
 
-
-def get_install_requires():
-    """
-    parse requirements.txt, ignore links, exclude comments
-    """
-    requirements = list()
-    for line in open('requirements.txt').readlines():
-        # skip to next iteration if comment, Git repository or empty line
-        if line.startswith('#') or line.startswith('git+https') or line == '':
-            continue
-        # add line to requirements
-        requirements.append(line.rstrip())
-
-    return requirements
-
 setup(
     name=name,
     version=version,
@@ -32,6 +17,6 @@ setup(
     author='ExCiteS',
     author_email='excites@ucl.ac.uk',
     packages=find_packages(exclude=['*.tests', '*.tests.*', 'tests.*']),
-    install_requires=get_install_requires(),
     include_package_data=True,
+    install_requires=['qrcode==5.2.2'],
 )
